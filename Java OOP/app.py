a76a81be5ad04575085dbaea0eb98496df3376f9
thasvithu <thasvithu7@gmail.com>
2024-02-13 16:36:31 +0530

    day2

--- Java OOP/app.py
+++ Java OOP/app.py
@@ -6,10 +6,23 @@ class App:
     name = "Vithusan"
 
     #Creating Constructor
-    def __init__(self, id, nic):
-        #instance Variables
-        self.id = id
-        self.nic = nic
+    def __init__(self):
+        print("Iam Constructor!")
+
+        #Instance/Object Variables
+        self.id = 6089
+        self.nic = "123456789"
+
+    """    
+    #Print Object Deatils
+    def __str__(self):
+        return "This is an App object"
+    """
 
 #Creating Objects to App class
-object = App(89, 123456789)
+object1 = App()
+
+# print(object1)
+
+#Print instance Variables
+print(id)
--- Python OOP/Main.java
+++ Python OOP/Main.java
@@ -6,18 +6,34 @@ public class Main {
     static String name = "Vithusan";
 
     //Instance Variables
-    int id;
-    String nic;
+    int id = 6089;
+    String nic = "123456789";
 
     //Creating Constructor
     public Main() {
-        System.out.println(age);
+        System.out.println("Hello iam Constructor!");
     }
+    
+    /*
+    //Print Object Deatils
+    public String toString() {
+        return "This is Main Object";
+    }
+    */
 
     //Starting point of the program
     public static void main(String[] args) {
         //Creating Objects to Main class
-        Main object = new Main();
+        Main object1 = new Main();
+
+        /*
+        //Print Object
+        System.out.println(object1);
+        System.out.println(object2);
+        */
+
+        //Print instance Variables
+        System.out.println("ID : " + object1.id);
     }
 }
 
